5b455bd5a6a7d72cbf7cdf407ae5a3a8859a7307
Alkazuz <kerallis2015@gmail.com>
2023-08-17 01:06:43 -0300

    feat: added HttpCode

--- src/exceptions/Handler.ts
+++ src/exceptions/Handler.ts
@@ -1,15 +1,16 @@
 import { NextFunction, Request, Response } from 'express'
 import { ModelNotFoundException } from './ModelNotFoundException'
 import { ENVIROMENT } from '../constants'
+import { HttpCode } from '../constants/HttpCode'
 
 export function handleErrors(error: unknown, req: Request, res: Response, next: NextFunction) {
 	if (error instanceof ModelNotFoundException) {
-		return defaultHandler(error, 404, res)
-	}   
+		return defaultHandler(error, HttpCode.NOT_FOUND, res)
+	}
 	next(error)
 }
 
-function defaultHandler(error: Error, status: number, res: Response) {
+function defaultHandler(error: Error, status: HttpCode, res: Response) {
 	let data = {
 		status: status,
 		exception: error.name,
--- src/http/controllers/Controller.ts
+++ src/http/controllers/Controller.ts
@@ -1,7 +1,8 @@
 import { Response } from 'express'
+import { HttpCode } from '../../constants/HttpCode'
 
 export default class Controller {
-	sendResponse(res: Response, data: unknown, status: number = 200) {
+	sendResponse(res: Response, data: unknown, status: HttpCode = 200) {
 		return res.status(status).json(data)
 	}
 }
